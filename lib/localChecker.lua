168f3a551bdb34b2ea0fc8f4c977fc014b5da33b
GN68 <81744903+GN68@users.noreply.github.com>
2026-05-16 00:26:41 +0800

    updated local checker and model utils

--- lib/localChecker.lua
+++ lib/localChecker.lua
@@ -1,41 +1,53 @@
+--[[______   __
+  / ____/ | / / Name: GN LOCAL CHECKER SERVICE v1.0.0
+ / / __/  |/ /  Desc: a simple script to check if your avatar is local on non host clients.
+/ /_/ / /|  / Author: GNanimates | https://gnon.top | @gn68s
+\____/_/ |_/ License: Mozilla Public License Version 2.0
+--────────-< DEPENDENCIES >-────────--
+Place required dependencies in the same folder as this script.
+- GNEvent > https://github.com/lua-gods/GNs-Avatar-5/blob/future/lib/GNEvent.lua
+]]
+
 local Event = require("lib.GNEvent")
 
-local api = {
+
+--- how many ticks to wait before checking if your avatar is local
+-- defaults to 60 ticks (3 second)
+local INTERVAL = 60
+--- the number of ticks in advance to send a heartbeat to avoid desync gaps.
+-- defaults to 20 ticks (1 second ahead)
+local ADVANCE = 20
+
+
+local LocalCheckerAPI = {
 	changed = Event.new(),
 	isLocal = false,
 }
 
-local INTERVAL = 3 * 20
-local DELAY = 20
-
 local life = INTERVAL
-
-function pings.pulse()
-	life = INTERVAL
-end
-
+function pings.gnlocalcheckerheartbeat() life = INTERVAL end
 
 events.WORLD_TICK:register(function ()
 	life = life - 1
 	if life <= 0 then
-		if not api.isLocal then
-			api.isLocal = true
-			api.changed:invoke(true)
+		if not LocalCheckerAPI.isLocal then
+			LocalCheckerAPI.isLocal = true
+			LocalCheckerAPI.changed:invoke(true)
 		end
 	else
-		if api.isLocal then
-			api.isLocal = false
-			api.changed:invoke(false)
+		if LocalCheckerAPI.isLocal then
+			LocalCheckerAPI.isLocal = false
+			LocalCheckerAPI.changed:invoke(false)
 		end
 	end
 end)
 
-if not host:isHost() then return api end
+if not host:isHost() then return LocalCheckerAPI end
 
 events.WORLD_TICK:register(function ()
-	if life < DELAY then
-		pings.pulse()
+	if life < ADVANCE then
+		pings.gnlocalcheckerheartbeat()
 	end
 end)
 
-return api
+return LocalCheckerAPI
--- lib/modelUtils.lua
+++ lib/modelUtils.lua
@@ -1,4 +1,16 @@
-local utils = {}
+--[[______   __
+  / ____/ | / / Name: GN MODEL UTILS LIBRARY v1.1.0
+ / / __/  |/ /  Desc: a script with some useful functions for working with models
+/ /_/ / /|  / Author: GNanimates | https://gnon.top | @gn68s
+\____/_/ |_/ License: Mozilla Public License Version 2.0 ]]
+---@class GN.ModelUtils
+local ModelUtils = {}
+
+local function isModel(model)
+	if not type(model) then
+		error("expected ModelPart, got "..type(model),2)
+	end
+end
 
 local function deepCopy(part)
 	local copy = part:copy(part:getName())
@@ -12,14 +24,17 @@ local function deepCopy(part)
 	return copy
 end
 
-
-function utils.shallowCopy(part)
-	local copy = part:copy(part:getName())
-	for key, value in pairs(part:getTask()) do
+-- duplicates a model while keeping its children as references to the original.  
+-- this exists because normal copy dosent copy over render tasks while this one does.
+function ModelUtils.shallowCopy(modelPart)
+	isModel(modelPart)
+	local copy = modelPart:copy(modelPart:getName())
+	for key, value in pairs(modelPart:getTask()) do
 		copy:addTask(value)
 	end
 	return copy
 end
+
 
 
 ---@param modelPart ModelPart
@@ -33,20 +48,23 @@ local function apply(modelPart,func)
 	end
 end
 
+
+---Applies a callback function to all the model's children.
 ---@param modelPart ModelPart
 ---@param func fun(modelPart:ModelPart): boolean?
 ---@return ModelPart
-function utils.apply(modelPart,func)
+function ModelUtils.apply(modelPart,func)
+	isModel(modelPart)
 	return apply(modelPart,func)
 end
 
 
----comment
----@param model ModelPart
-function utils.deepCopy(model)
-	assert(type(model) == "ModelPart", "expected ModelPart, got "..type(model))
-	return deepCopy(model)
+--- duplicates the model recursively. with no references to the original
+---@param modelPart ModelPart
+function ModelUtils.deepCopy(modelPart)
+	isModel(modelPart)
+	return deepCopy(modelPart)
 end
 
 
-return utils
+return ModelUtils
