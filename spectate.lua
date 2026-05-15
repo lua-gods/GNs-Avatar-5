3a649805fa0fae38c367a73d4261654e87a26f40
GN68 <81744903+GN68@users.noreply.github.com>
2026-05-16 01:41:57 +0800

    cleaned up

--- spectate.lua
+++
@@ -1,9 +0,0 @@
-local wasSpectating = false
-events.TICK:register(function ()
-	local isSPectating = player:getGamemode() == "SPECTATOR"
-	if wasSpectating ~= isSPectating then
-		wasSpectating = isSPectating
-		models.player:setVisible(not isSPectating)
-		nameplate.ENTITY:setVisible(not isSPectating)
-	end
-end)
