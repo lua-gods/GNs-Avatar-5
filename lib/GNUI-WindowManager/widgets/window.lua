7a16b5fc0b81b90695b65014a2592ee3959b5d99
GN68 <81744903+GN68@users.noreply.github.com>
2026-05-17 02:46:11 +0800

    more Window API APIs

--- lib/GNUI-WindowManager/widgets/window.lua
+++ lib/GNUI-WindowManager/widgets/window.lua
@@ -5,86 +5,103 @@
 \____/_/ |_/ source: link ]]
 
 
-local BASE = ((...):gsub("/",".")):match(".+%.GNUI")
-local cfg = require(BASE..".config") ---@type GNUI.config
-local Box = require(cfg.WIDGETS..".box") ---@type GNUI.BoxAPI
+local BASE = ((...):gsub("/", ".")):match(".+%.GNUI")
+local cfg = require(BASE .. ".config") ---@type GNUI.config
+local Box = require(cfg.WIDGETS .. ".box") ---@type GNUI.BoxAPI
 local Event = require(cfg.EVENT)
-local Button = require(cfg.WIDGETS..".button") ---@type GNUI.Widget.ButtonAPI
+local Button = require(cfg.WIDGETS .. ".button") ---@type GNUI.Widget.ButtonAPI
 
-local Style = require(cfg.THEME..".init") ---@type GNUI.ThemeAPI
-local Layout = require(cfg.LAYOUT..".init") ---@type GNUI.LayoutAPI
+local Theme = require(cfg.THEME .. ".init") ---@type GNUI.ThemeAPI
+local Layout = require(cfg.LAYOUT .. ".init") ---@type GNUI.LayoutAPI
 
 local utils = require(cfg.UTILS) ---@type GNUI.utils
 
+---@class GNUI.WindowAPI
+local WindowAPI = {}
 
----@class GNUI.Widget.WindowAPI : GNUI.Box
+---@class GNUI.Widget.Window : GNUI.Box
 local Window = {}
-Window.__index = Window
+Window.__index = function(t, i)
+	return rawget(t, i)
+		 or Window[i]
+		 or Box.index(t, i)
+end
 
 
-function Window.new(canvas)
+
+
+
+---@param canvas GNUI.Canvas
+---@return GNUI.Widget.Window
+function WindowAPI.new(canvas)
 	local self = Layout.parse(canvas, {
 		layout = "VERTICAL",
 		sizing = { "FIXED", "FIXED" },
-		
+
 		{
 			{
-				layout="HORIZONTAL",
-				variant="none",
-				gap=1,
-				sizing={"FILL", "FIT"},
-				margin=vec(0, 0, 0, 0),
+				layout = "HORIZONTAL",
+				style = "none",
+				gap = 1,
+				sizing = { "FILL", "FIT" },
+				margin = vec(0, 0, 0, 0),
 				{
 					{
-						type="button",
-						name="titlebar",
-						text="title",
-						sizing =  {"FILL", "FIT"}
+						type = "button",
+						name = "titlebar",
+						text = "title",
+						sizing = { "FILL", "FIT" },
 					},
 					{
-						type="button",
-						name="close",
-						text="x",
-						sizing =  {"FIT", "FIT"}
-					}
-				}
+						type = "button",
+						name = "close",
+						text = "x",
+						sizing = { "FIT", "FIT" },
+					},
+				},
 			},
 			{
-				name="content",
-				variant="none",
-				sizing =  {"FILL", "FILL"}
-			}
-		}
+				name = "content",
+				style = "none",
+				sizing = { "FILL", "FILL" },
+			},
+		},
 	})
-	
+
 	local titlebar = self:getChild("titlebar")
 	---@cast titlebar GNUI.Widget.Button
 	titlebar.BUTTON_DOWN:register(function()
-		self.canvas.CURSOR_MOVED:register(function (pos, vel)
+		self.canvas.CURSOR_MOVED:register(function(pos, vel)
 			self:setPos(self.pos + vel)
-		end,titlebar.id)
+		end, titlebar.id)
 	end)
-	
-	titlebar.BUTTON_UP:register(function ()
+
+	titlebar.BUTTON_UP:register(function()
 		self.canvas.CURSOR_MOVED:remove(titlebar.id)
 	end)
-	
+
 	local close = self:getChild("close")
 	---@cast close GNUI.Widget.Button
 	close.BUTTON_DOWN:register(function()
 		self:free()
 	end)
-	
+
+	setmetatable(self,Window)
+	---@cast self GNUI.Widget.Window
 	return self
 end
 
-
 ---@param child GNUI.Box
----@return GNUI.Widget.WindowAPI
-function Window:addChild(child)
+---@return GNUI.Widget.Window
+function Window:addContent(child)
 	self:getChild("content"):addChild(child)
 	return self
 end
 
 
-return Window
+function Window:setTitle(title)
+	self:getChild("titlebar"):setText(title)
+end
+
+
+return WindowAPI
