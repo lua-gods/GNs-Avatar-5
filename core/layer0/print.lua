86e57549beca1bddfdabcfeb6f0f8b5c501fd5c7
GN68 <81744903+GN68@users.noreply.github.com>
2026-06-02 17:54:25 +0800

    cool new print

--- core/layer0/print.lua
+++ core/layer0/print.lua
@@ -2,6 +2,55 @@ local function getPath()
 	return select(2, pcall(function() error("", 1) end)):match("^([^\n:]*):")
 end
 
+
+function print(...)
+	local content = {
+		{text=""}
+	}
+	local varags = {...}
+	local varagSize = #varags
+	for index, value in ipairs(varags) do
+		if type(value) == "table" then
+			content[#content+1] = {
+				text = tostring(value),
+				color="#78bfff",
+				hoverEvent={
+					action="show_text",
+					value=printTable(value,1,true):gsub("\t","  ")
+				}
+			}
+		else
+			content[#content+1] = {
+				text = tostring(value)
+			}
+		end
+		if varagSize ~= index then
+			content[#content+1] = {
+				text = " "
+			}
+		end
+	end
+	printJson(toJson{
+		{
+			color="white",
+			font="minecraft:mono",
+			text=""
+		},
+		{
+			text="[lua] ",
+			color="#5555FF",
+		},
+		{
+			text="",
+			extra=content
+		},
+		{
+			text="\n"
+		}
+	})
+end
+
+
 function warn(...)
 	local ok, result = pcall(toJson,{
 		{
