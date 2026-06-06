e33190113d1245a40c8223aa939760c53c0bd567
GN68 <81744903+GN68@users.noreply.github.com>
2026-06-06 17:19:18 +0800

    zzz

--- auto/zzz.lua
+++ auto/zzz.lua
@@ -4,9 +4,15 @@ local zzzSpawner = GNParticles.registerIdentity(
 	"zzz",
 	GNParticles.newBillboardTexture(textures.particles,9,1,7,7),
 	50,function (dust)
+		dust.vel = vec(0,0.05,0)
 		dust.pos = dust.pos + dust.vel
-		dust.
+		local t = dust.age * 0.1
+		dust.pos.x = dust.pos.x + math.sin(t) * 0.05
+		dust.pos.z = dust.pos.z + math.cos(t) * 0.05
 	end
 )
 
-zzzSpawner:spawn(vec(0,0,0),vec(0,0.1,0))
+--TODO: add scale property
+--TODO: add init callback to identity
+
+zzzSpawner:spawn(vec(0,0,0))
