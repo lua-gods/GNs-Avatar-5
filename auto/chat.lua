504d055f3b78d33e09a87b27a6981e76bea8c76d
GN68 <81744903+GN68@users.noreply.github.com>
2026-09-03 23:42:05 +0800

    cleaned up GN chat bubble

--- auto/chat.lua
+++ auto/chat.lua
@@ -12,22 +12,29 @@ Place required dependencies in the same folder as this script.
 local modelUtils = require("lib.modelUtils")
 local Tween = require("lib.GNtween")
 
+--──── CONFIG ────────────────────────────────────────────--
+
 local MODEL = models.bubble.bubble
-local MODEL_ALT = models.bubble_alt.bubble
 local MAX_WIDTH = 128+32
-local PADDING = 1
+local PADDING = 3
 local CHILD_GAP = 4
 local MASTER_VOLUME = 0.5
 local MASTER_SCALE = 0.3
 
+--──── END OF CONFIG ────────────────────────────────────────────--
+
 local scroll = 0
 
-MODEL:setVisible(false)
-MODEL_ALT:setVisible(false)
 
 local modelOrigin = models:newPart("bubbleOrigin")
 local modelCamera = modelOrigin:newPart("bubbleCamera", "CAMERA")
 local modelScroll = modelCamera:newPart("scroll")
+local lastBubbleModel
+
+
+MODEL:setVisible(false)
+modelOrigin:setPos(0, 32 + 8, 0)
+modelOrigin:scale(MASTER_SCALE)
 
 
 ---@param id Minecraft.soundID
@@ -38,12 +45,6 @@ local function sound(id, pitch, volume)
 		return sounds:playSound(id, player:getPos(), volume * MASTER_VOLUME, pitch)
 	end
 end
-
-
-modelOrigin:setPos(0, 32 + 8, 0)
-modelOrigin:scale(MASTER_SCALE)
-
-local lastBubbleModel
 
 
 local function fade(bubble)
@@ -64,8 +65,9 @@ local function fade(bubble)
 	}
 end
 
-local function newBubble(text,alt)
-	local model = modelUtils.deepCopy(alt and MODEL_ALT or MODEL)
+
+local function newBubble(text)
+	local model = modelUtils.deepCopy(MODEL)
 	model:setVisible(true)
 	model:setLight(15, 15)
 		 :setPrimaryRenderType("CUTOUT_EMISSIVE_SOLID")
@@ -76,22 +78,18 @@ local function newBubble(text,alt)
 	local label = model.LBL:newText("label")
 
 	label:setAlignment("LEFT")
-		 :setText(text)
-		 :width(MAX_WIDTH)
+		 :setText(('{"text":"%s","color":"white"}'):format(text))
+		 :width(MAX_WIDTH+3)
 		 :light(15, 15)
 		 :setVisible(true)
-
-	local size = client.getTextDimensions(text:gsub(":[^a-zA-Z_]+:", "W"), MAX_WIDTH, true):ceil()
-		 :add(1, 0)
-		 :add(PADDING, PADDING)
-	size.x = math.max(size.x, 8)
-	size.x = math.ceil(size.x / 2) * 2
-
+	local size = client.getTextDimensions(text:gsub(":[^:]+:", "_l"),MAX_WIDTH+3,true):ceil()
+		 :sub(1, 0) -- removes excess space at the end of text
+		 :add(PADDING*2, PADDING)
 
 	-- I am too lazy ok
 	local function setSize(size)
 		local h = size.x * 0.5 - 0.5
-		label:pos(h, size.y - 1 - PADDING * 0.5)
+		label:pos(h+0.5-PADDING, size.y - 1 - PADDING * 0.5)
 		model.BL:pos(h, 0):scale(1, 1, 0)
 		model.BM:pos(0, 0):scale(size.x, 1, 0)
 		model.BR:pos(-h, 0):scale(1, 1, 0)
@@ -114,7 +112,7 @@ local function newBubble(text,alt)
 
 	Tween.new {
 		from = scroll,
-		to = scroll + size.y + CHILD_GAP,
+		to = scroll + size.y + PADDING + CHILD_GAP,
 		duration = 0.15,
 		easing = "outSine",
 		tick = function(v, t)
@@ -124,6 +122,9 @@ local function newBubble(text,alt)
 		end,
 	}
 	model:setVisible(true)
+	label:setVisible(false)
+	
+	-- the animation that plays when the speech bubble expands
 	Tween.new {
 		from = vec(8, 0),
 		to = size,
@@ -131,11 +132,8 @@ local function newBubble(text,alt)
 		easing = "outSine",
 		tick = function(v, t)
 			setSize(v)
-			label:width(v.x)
-			label:setVisible(false)
 		end,
 		onFinish = function()
-			label:setVisible(true)
 			label:setVisible(true)
 		end,
 	}
@@ -147,8 +145,6 @@ local function newBubble(text,alt)
 	}
 
 	Tween.new {
-		from = 0,
-		to = 0,
 		duration = 5,
 		onFinish = function()
 			fade(bubble)
@@ -158,25 +154,7 @@ local function newBubble(text,alt)
 	return bubble
 end
 
---[[
-local bubble = newSpeech("ooo")
-
-local clock = 0
-local time = 0
-events.TICK:register(function ()
-	clock = clock + 1
-	if clock > 3 then
-		clock = 0
-		time = (time + 1) % 6
-		local str = ""
-		for i = 0, 2, 1 do
-			str = str .. ((time == i) and "O" or "o")
-		end
-		bubble.label:setText(str)
-	end
-end)
---]]
-
+--──── BASIC ENCRYPTION ────────────────────────────────────────────--
 
 local function getKey(alt)
 	return math.floor(world.getTimeOfDay() / 12000 + (alt and 1 or 0))
@@ -215,74 +193,27 @@ local function decrypt(text)
 	return result:sub(#HEADER + 1, -1)
 end
 
+--──── FIGURA HOOKS ────────────────────────────────────────────--
 
-function pings.chat(msg, alt)
-	if alt then
-		local total = math.max(#msg,1)
-		newBubble('{"text":"'..(("g"):rep(total*2))..'","font":"minecraft:alt","obfuscated":true,"color":"#00ff00"}',alt)
-		--[ [
-		local count = 0
-		local timer = 0
-		local lastSound
-		local lastSound2
-		
-		local function process()
-			timer = timer + 1
-			if timer > 2 then
-				timer = 0
-				if lastSound then
-					lastSound:stop()
-					lastSound2:stop()
-				end
-				lastSound = sound("minecraft:block.note_block.bit", math.lerp(0.02,0.2,math.random()), 1)
-				lastSound2 = sound("minecraft:block.note_block.bit", math.lerp(0.02,0.2,math.random()), 1)
-				count = count + 1
-				if count > total then
-					lastSound:stop()
-					lastSound2:stop()
-					sound("minecraft:block.note_block.bit", 0.1, 1)
-					events.TICK:remove(process)
-				end
-			end
-		end
-		events.TICK:register(process)
-		--]]
-		
-		
-		--[[
-		local s = sound("minecraft:block.note_block.bit",1,1)
-		Tween.new {
-			from = 1,
-			to = 0.5,
-			duration = 1,
-			tick = function (v, t)
-				local r = math.lerp(0.2,v,math.random())
-				s:pitch(r)
-			end,
-		}
-		--]]
-	else
-		msg = msg:gsub("\\", "\n")
-		newBubble('{"text":"' .. decrypt(msg) .. '","color":"white"}')
-		sound("minecraft:entity.item.pickup", 0.35, 0.5)
-		Tween.new {
-			from = 0,
-			to = 0,
-			duration = 0.15,
-			onFinish = function()
-				sound("minecraft:entity.item.pickup", 0.7, 0.5)
-			end,
-		}
-	end
+function pings.chat(msg)
+	msg = msg:gsub("\\", "\n")
+	newBubble(decrypt(msg))
+	
+	-- the sound that plays when a bubble appears
+	sound("minecraft:entity.item.pickup", 0.35, 0.5)
+	Tween.new {
+		from = 0,
+		to = 0,
+		duration = 0.15,
+		onFinish = function()
+			sound("minecraft:entity.item.pickup", 0.7, 0.5)
+		end,
+	}
 end
 
 events.CHAT_SEND_MESSAGE:register(function(message)
-	if message then
-		if message:find("^/") then
-			pings.chat(("a"):rep(#message*0.25),true)
-		else
-			pings.chat(encrypt(message))
-		end
-		return message
+	if message and not message:find("^/") then
+		pings.chat(encrypt(message))
 	end
+	return message
 end)
